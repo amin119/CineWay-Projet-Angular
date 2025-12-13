fd56a29c26e575bbb56603c1a3b2b4a62731e20c
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-12-13 20:32:43 +0100

    movie card added

---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,26 @@
+// this is not the final version of the movie model
+export interface MovieModel {
+  id: number;
+  title: string;
+  description: string;
+  duration_minutes: number;
+  genre: string[];
+  rating: string;
+  cast: string[];
+  director: string;
+  writers: string[];
+  producers: string[];
+  release_date: string;
+  country: string;
+  language: string;
+  budget: number;
+  revenue: number;
+  production_company: string;
+  distributor: string;
+  image_url: string;
+  trailer_url: string;
+  awards: string[];
+  details: Record<string, any>;
+  created_at: string;
+  updated_at: string;
+}
