060b9b17b4735ad3d9a25395ac24d0a76b39ab09
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-12-16 23:27:36 +0100

    not found page added ; error handling in cinemas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,107 +2,58 @@ import { Routes } from '@angular/router';
 import { Signup } from './auth/signup/signup';
 import { Login } from './auth/login/login';
 import { Home } from './components/home/home';
-
-import { authGuard } from './auth/guards/auth.guard';
-import { guestGuard } from './auth/guards/guest.guard';
-import { adminGuard } from './auth/guards/admin/admin.guard';
-
 import { LandingPageComponent } from './components/landingPage/landing-page-component/landing-page-component';
 import { MainLayout } from './components/layouts/main-layout/main-layout';
 import { Cinemas } from './components/cinemas/cinemas/cinemas';
 import { CinemaDetails } from './components/cinemas/cinema-details/cinema-details';
 import { Profile } from './components/profile/profile';
-
 import { AdminLayoutComponent } from './components/admin/layouts/admin-layout/admin-layout';
 import { AdminOverviewComponent } from './components/admin/pages/admin-overview/admin-overview';
 import { AdminMoviesComponent } from './components/admin/pages/admin-movies/admin-movies';
 import { AdminCinemasComponent } from './components/admin/pages/admin-cinemas/admin-cinemas';
 import { AdminUsersComponent } from './components/admin/pages/admin-users/admin-users';
 import { AdminShowtimesComponent } from './components/admin/pages/admin-showtimes/admin-showtimes';
+import { authGuard } from './auth/guards/auth.guard';
+import { guestGuard } from './auth/guards/guest.guard';
+import { adminGuard } from './auth/guards/admin/admin.guard';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayout,
+    canActivate: [authGuard],
     children: [
-      {
-        path: 'home',
-        component: Home,
-        canActivate: [authGuard],
-      },
-      {
-        path: '',
-        component: LandingPageComponent,
-        canActivate : [guestGuard]
-      },
-      {
-        path: 'cinemas',
-        component : Cinemas,
-        canActivate :[authGuard],
-      },
-      {
-        path: 'cinemas/:id',
-        component: CinemaDetails,
-        canActivate: [authGuard],
-      },
-      {
-        path: 'profile',
-        component: Profile,
-        canActivate: [authGuard],
-      }
+      { path: 'home', component: Home },
+      { path: 'cinemas', component: Cinemas },
+      { path: 'cinemas/:id', component: CinemaDetails },
+      { path: 'profile', component: Profile },
+      { path: 'not-found', component: NotFound },
     ],
   },
-
+  {
+    path: '',
+    canActivate: [guestGuard],
+    children: [
+      { path: '', component: LandingPageComponent },
+      { path: 'auth/login', component: Login },
+      { path: 'auth/signup', component: Signup },
+    ],
+  },
   {
     path: 'admin',
     component: AdminLayoutComponent,
     canActivate: [adminGuard],
     children: [
-      {
-        path: 'overview',
-        component: AdminOverviewComponent,
-      },
-      {
-        path: '',
-        redirectTo: 'overview',
-        pathMatch: 'full',
-      },
-      {
-        path: 'movies',
-        component: AdminMoviesComponent,
-      },
-      {
-        path: 'cinemas',
-        component: AdminCinemasComponent,
-      },
-      {
-        path: 'users',
-        component: AdminUsersComponent,
-      },
-      {
-        path: 'showtimes',
-        component: AdminShowtimesComponent,
-      },
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
+      { path: 'overview', component: AdminOverviewComponent },
+      { path: 'movies', component: AdminMoviesComponent },
+      { path: 'cinemas', component: AdminCinemasComponent },
+      { path: 'users', component: AdminUsersComponent },
+      { path: 'showtimes', component: AdminShowtimesComponent },
     ],
   },
-
   {
-    path: 'auth',
-    canActivate: [guestGuard],
-    children: [
-      {
-        path: 'home',
-        redirectTo: 'login',
-        pathMatch: 'full',
-      },
-      {
-        path: 'login',
-        component: Login,
-      },
-      {
-        path: 'signup',
-        component: Signup,
-      },
-    ],
-  },
+    path: "**" , redirectTo:'/not-found'
+  }
 ];
--- src/app/components/cinemas/cinema-details/cinema-details.ts
+++ src/app/components/cinemas/cinema-details/cinema-details.ts
@@ -1,28 +1,26 @@
-import { Component, inject, signal, computed } from '@angular/core';
+import { Component, inject, signal, computed, OnInit, effect } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Cinema } from '../../../models/cinema.model';
-import {
-  formatName,
-  getIconAmenity,
-} from '../../../config/amenities.config';
+import { formatName, getIconAmenity } from '../../../config/amenities.config';
 import { httpResource } from '@angular/common/http';
 import { APP_API } from '../../../config/app-api.config';
 import { APP_ROUTES } from '../../../config/app-routes.confg';
 import { Showtimes } from '../showtimes/showtimes';
 import { MovieModel } from '../../../models/movie.model';
-import { Movie } from "../../movies/movie/movie";
+import { Movie } from '../../movies/movie/movie';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cinema-details',
   templateUrl: './cinema-details.html',
   styleUrl: './cinema-details.css',
-  imports: [Showtimes, Movie]
+  imports: [Showtimes, Movie],
 })
 export class CinemaDetails {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private cinemaId = signal(Number(this.route.snapshot.paramMap.get('id')));
-
+  private toastr = inject(ToastrService);
   readonly cinemaDetailsRes = httpResource<Cinema>(() => ({
     url: `${APP_API.cinema.list}${this.cinemaId()}`,
     method: 'GET',
@@ -30,7 +28,10 @@ export class CinemaDetails {
   cinema = computed(() => this.cinemaDetailsRes.value());
   error = computed(() => this.cinemaDetailsRes.error());
   isLoading = computed(() => this.cinemaDetailsRes.isLoading());
-
+  hasValue = computed(() => this.cinemaDetailsRes.hasValue());
+  // taaml http request hata kif cinema moush mawjoud : lezem tetbadel ma taamel request ken baad cinema
+  // tetbadel : ma tjibhomch lkol : tant que ma nzelch ala see more wela faza limit hia 6 movies
+  // kif kif lel movies showtimes
   readonly cinemaMovies = httpResource<MovieModel[]>(() => ({
     url: `${APP_API.cinema.list}${this.cinemaId()}/movies`,
     method: 'GET',
@@ -38,7 +39,14 @@ export class CinemaDetails {
   movies = computed(() => this.cinemaMovies.value());
   moviesError = computed(() => this.cinemaMovies.error());
   isLoadingMovies = computed(() => this.cinemaMovies.isLoading());
-
+  constructor() {
+    effect(() => {
+      if (!this.isLoading() && this.error()) {
+        this.router.navigate([APP_ROUTES.cinemas]);
+        this.toastr.error('An error occurred while loading the cinema details');
+      }
+    });
+  }
   getIcon(amenity: string): string {
     return getIconAmenity(amenity);
   }
@@ -50,7 +58,7 @@ export class CinemaDetails {
     this.router.navigate([APP_ROUTES.cinemas]);
   }
 
-    getCinemaDetails(cinema_id: number) {
+  getCinemaDetails(cinema_id: number) {
     this.cinemaId.update(() => cinema_id);
   }
 }
