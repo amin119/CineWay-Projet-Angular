5a6394b38c7190542ede1efe6bc54140938b60dc
eyamhamdi03 <eya.mhamdi@insat.ucar.tn>
2025-12-16 20:27:24 +0100

    merge conflicts solved

--- src/app/components/navbar-component/navbar-component.ts
+++ src/app/components/navbar-component/navbar-component.ts
@@ -18,7 +18,7 @@ private router = inject(Router);
 private toastrService = inject(ToastrService);
 APP_ROUTES = APP_ROUTES;
 menuOpen = false;
-  userApi=inject(UserApi)
+userApi=inject(UserApi)
 
 toggleMenu() {
   this.menuOpen = !this.menuOpen;
@@ -31,11 +31,5 @@ logout(){
 isAuth(){
   return this.authService.isAuthenticated();
 }
-<<<<<<< HEAD
   user = this.userApi.user;
-=======
-user = this.authService.getUser();
->>>>>>> 1a9ae4c82d7a4be20d80ce8f97f2795c3704fd0e
-
-
 }
